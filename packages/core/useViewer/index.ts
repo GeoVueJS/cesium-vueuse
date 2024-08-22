66d461cbed720799ac7c94816a2f711ec549459c
xiankq <840442707@qq.com>
2024-08-22 20:16:00 +0800

    fix: 修复Viewer获取逻辑

--- packages/core/useViewer/index.ts
+++ packages/core/useViewer/index.ts
@@ -1,4 +1,4 @@
-import { computed, getCurrentInstance, inject } from 'vue';
+import { getCurrentInstance, inject, shallowReadonly } from 'vue';
 
 import { CREATE_VIEWER_COLLECTION, CREATE_VIEWER_INJECTION_KEY } from '../createViewer';
 
@@ -6,23 +6,16 @@ import type { Viewer } from 'cesium';
 import type { ShallowRef } from 'vue';
 
 /**
- * 获取父级inject的`Viewer`对象
+ * 获取当前组件或祖先级组件通过`createViewer`注入的Viewer对象
  */
 export function useViewer(): Readonly<ShallowRef<Viewer | undefined>> {
-  const injectViewer = inject(CREATE_VIEWER_INJECTION_KEY);
   const instance = getCurrentInstance();
 
-  const viewer = computed(() => {
-    if (instance) {
-      return CREATE_VIEWER_COLLECTION.get(instance)?.value;
-    }
-    else {
-      return injectViewer?.value;
-    }
-  });
+  const injectViewer = inject(CREATE_VIEWER_INJECTION_KEY);
+  const instanceViewer = instance ? CREATE_VIEWER_COLLECTION.get(instance) : undefined;
 
-  if (!instance || CREATE_VIEWER_COLLECTION.get(instance) || injectViewer) {
+  if (!instanceViewer || !injectViewer) {
     console.warn('未检测到viewer实例的相关注入');
   }
-  return viewer!;
+  return instanceViewer ? shallowReadonly(instanceViewer)! : injectViewer!;
 }
