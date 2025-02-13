031373cc538b0b940e071f5bd87711892d8abbe2
xiankq <xiankq@qq.com>
2025-02-13 19:29:42 +0800

    build(vite): add CDN import for production build
    - Add vite-plugin-cdn-import to package.json
    - Configure CDN import for vue, vue-router, pinia, and cesium in vite.config.ts
    - Update pnpm-lock.yaml with new dependencies
    
    This change will improve performance by loading some libraries from CDN in production builds.

--- packages/.vitepress/vite.config.ts
+++ packages/.vitepress/vite.config.ts
@@ -4,24 +4,47 @@ import UnoCSS from 'unocss/vite';
 import UnpluginCesium from 'unplugin-cesium/vite';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
+import Cdn from 'vite-plugin-cdn-import';
 import VueDevTools from 'vite-plugin-vue-devtools';
 
 const CESIUM_VERSION = (await getPackageInfo('cesium'))!.version;
 
-export default defineConfig({
-  plugins: [
-    UnpluginCesium({
-      copyStaticFiles: false,
-      cesiumBaseUrl: `https://cdn.jsdelivr.net/npm/cesium@${CESIUM_VERSION}/Build/Cesium/`,
-    }),
-    VueDevTools(),
-    UnoCSS(),
-    Components({
-      dirs: fileURLToPath(new URL('./theme/components', import.meta.url)),
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: fileURLToPath(new URL('./components.d.ts', import.meta.url)),
-    }),
-  ],
-  server: { port: 9574 },
-  publicDir: '.vitepress/public',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      UnpluginCesium({
+        copyStaticFiles: false,
+        cesiumBaseUrl: `https://cdn.jsdelivr.net/npm/cesium@${CESIUM_VERSION}/Build/Cesium/`,
+      }),
+      VueDevTools(),
+      UnoCSS(),
+      Components({
+        dirs: fileURLToPath(new URL('./theme/components', import.meta.url)),
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dts: fileURLToPath(new URL('./components.d.ts', import.meta.url)),
+      }),
+      ...(mode === 'build'
+        ? [
+            Cdn({
+              modules: [
+                'vue',
+                'vue-router',
+                {
+                  name: 'pinia',
+                  var: 'Pinia',
+                  path: `/dist/pinia.iife.prod.js`,
+                },
+                {
+                  name: 'cesium',
+                  var: 'Cesium',
+                  path: `/Build/Cesium/Cesium.min.js`,
+                },
+              ],
+            }),
+          ]
+        : []),
+    ],
+    server: { port: 9574 },
+    publicDir: '.vitepress/public',
+  };
 });
